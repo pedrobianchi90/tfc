80f37c5f5715ca7cf10fe29360a062cc5c4ca693
Pedro Bianchi <pedroedm@gmail.com>
2022-10-25 15:24:45 -0300

    Adiciona Migration e Model de Teams

---
+++ app/backend/src/database/models/TeamModel.ts
@@ -0,0 +1,28 @@
+import { INTEGER, Model, STRING } from 'sequelize';
+import db from '.';
+
+class Team extends Model {
+  public id!: number;
+  public teamName!: string;
+}
+
+Team.init({
+  id: {
+    type: INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  teamName: {
+    type: STRING,
+    allowNull: false,
+    field: 'team_name',
+  },
+}, {
+  sequelize: db,
+  modelName: 'Teams',
+  underscored: true,
+  timestamps: false,
+});
+
+export default Team;
